5d92370a3bea75a52d44b8ad4dcdbb43ddaa70cd
黄子超 <huangzichao@huangzichaodeMacBook-Pro.local>
2020-10-20 14:43:58 +0800

    修改和文件夹的提交

---
+++ first/src/main/java/com/huangzichao/controller/Mycontroller.java
@@ -0,0 +1,13 @@
+package com.huangzichao.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Mycontroller {
+
+
+    public void writeSomething(){
+      system.out.println("huangzichao_test");
+    }
+}
+
